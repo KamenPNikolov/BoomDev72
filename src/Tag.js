31445d5387068e7a5b9290721403676a005f7c6b
knikolov <kami_985@abv.bg>
2022-01-14 10:20:23 +0200

    Final attempt to make it work

--- src/Tag.js
+++ src/Tag.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Tag(props){
 
     return (<div className="tags">
-       <div className="tag">#{props.tags[0]||"default text"}</div> 
-       <div className="tag">#{props.tags[1]||"default text"}</div>
-       <div className="tag">#{props.tags[2]||"default text"}</div>
-       <div className="tag">#{props.tags[3]||"default text"}</div>
+       <div className="tag">#{props.tags[0]}</div> 
+       <div className="tag">#{props.tags[1]}</div>
+       <div className="tag">#{props.tags[2]}</div>
+       <div className="tag">#{props.tags[3]}</div>
        </div>);
 
 };
